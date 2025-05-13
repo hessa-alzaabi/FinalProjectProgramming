3334d11a0bf5e51b6f720d76b46f7a6cac7aeedb
hessa <hessa.alzaabi9@gmail.com>
2025-05-13 20:31:04 +0400

    commit

--- Models/ticket.py
+++ Models/ticket.py
@@ -1,23 +1,40 @@
+# Represents an event ticket
 class Ticket:
     def __init__(self, ticket_id, price, validity_start, validity_end, is_available=True):
-        self._ticket_id = ticket_id
-        self._price = price
-        self._validity_period = (validity_start, validity_end)
-        self._is_available = is_available
+        self._ticket_id = ticket_id  # Unique ID for this ticket
+        self._price = price  # How much the ticket costs
+        self._validity_period = (validity_start, validity_end)  # When ticket is valid
+        self._is_available = is_available  # Availability status
+
+    # Get the ticket ID
     def get_ticket_id(self):
         return self._ticket_id
+
+    # Get the ticket price
     def get_price(self):
         return self._price
+
+    # Get when this ticket is valid
     def get_validity_period(self):
         return self._validity_period
+
+    # Check if this ticket can be bought
     def is_available(self):
         return self._is_available
+
+    # Change the ticket price
     def set_price(self, price):
         self._price = price
+
+    # Change when the ticket is valid
     def set_validity_period(self, start, end):
         self._validity_period = (start, end)
+
+    # Mark ticket as available or sold
     def update_availability(self, available):
         self._is_available = available
+
+    # Show ticket information
     def display_details(self):
         start, end = self._validity_period
         return (
@@ -26,7 +43,10 @@ class Ticket:
             f"Validity: {start} to {end}, "
             f"Available: {'Yes' if self._is_available else 'No'}"
         )
+    #Get the name of the Ticket type
     def get_type_name(self):
         return "Ticket"
+
+    #Get the ticket event
     def get_event(self):
         return None
--- Models/user.py
+++ Models/user.py
@@ -1,5 +1,7 @@
+# Creates a new user with personal information
 class User:
     def __init__(self, user_id, username, password, salt, full_name, email, phone_number, address):
+        # Store all user details privately
         self._user_id = user_id
         self._username = username
         self._password = password
@@ -8,41 +10,76 @@ class User:
         self._email = email
         self._phone_number = phone_number
         self._address = address
-        self._purchase_history = []  # Changed to list
+        self._purchase_history = []  # Keep track of orders
+
+    # Get the user's ID
     def get_user_id(self):
         return self._user_id
+
+    # Get the username
     def get_username(self):
         return self._username
+
+    # Get the password
     def get_password(self):
         return self._password
+    #Get the salt used for hashing the user's password
     def get_salt(self):
-        return self._salt  # Corrected to use _salt
+        return self._salt
+
+    # Get the full name
     def get_full_name(self):
         return self._full_name
+
+    # Get the email address
     def get_email(self):
         return self._email
+
+    # Get the phone number
     def get_phone_number(self):
         return self._phone_number
+
+    # Get the address
     def get_address(self):
         return self._address
+
+    # Get the list of past orders
     def get_purchase_history(self):
         return self._purchase_history
+
+    # Update the username
     def set_username(self, username):
         self._username = username
+
+    # Update the password
     def set_password(self, password):
         self._password = password
+
+    # Update the full name
     def set_full_name(self, full_name):
         self._full_name = full_name
+
+    # Update the email address
     def set_email(self, email):
         self._email = email
+
+    # Update the phone number
     def set_phone_number(self, phone_number):
         self._phone_number = phone_number
+
+    # Update the address
     def set_address(self, address):
         self._address = address
+
+    # Register a new user in the system
     def register(self):
         return True
+
+    # Check if login details are correct
     def login(self, username, password):
         return self._username == username and self._password == password
+
+    # Update user's personal information
     def update_profile(self, full_name=None, email=None, phone_number=None, address=None):
         if full_name:
             self._full_name = full_name
@@ -52,7 +89,11 @@ class User:
             self._phone_number = phone_number
         if address:
             self._address = address
+
+    # Add an order to the user's purchase history
     def add_purchase(self, order):
         self._purchase_history.append(order)
+
+    # Show all previous purchases
     def view_purchase_history(self):
         return self._purchase_history
